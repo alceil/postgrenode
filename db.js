fb42d8eb056d22475e71deb4f54a29e919909387
alceil <ashishshajisrampickal@gmail.com>
2020-11-16 20:22:52 +0530

    added routes

--- db.js
+++ db.js
@@ -1,1 +1,10 @@
 const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user:"postgres",
+    password:"ilmvm123",
+    host:"localhost",
+    port:5433,
+    database:"perntodo"
+}) ;
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 app.use(cors());
 app.use(express.json);
 
+app.post('/todos',async (req,res)=>{
+    try {
+        const {description} = req.body;
+        const newTodo = await pool.query(
+            "INSERT  INTO todo (description) VALUES($1)",
+            [description]
+            );
+            res.json(newTodo);
+      
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+    
+
+});
 
 app.listen('5000',()=>{
     console.log('Server app running on port 5000');
